4bb0440af7583c41e09d91d7fe9130b3b3d8b051
Kevin Cox <kevincox.ca@gmail.com>
2013-08-27 13:04:36 -0400

    Added valid-array tests.

---
+++ spec/valid-array_spec.rb
@@ -0,0 +1,22 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe ValidArray do
+  context 'when custom validator is used' do
+    before :each do
+      class MyArray < Array
+        extend ValidArray
+        
+        def self.validate(i, e)
+          e.to_s
+        end
+      end
+      @typed_ary = MyArray.new(5) {|i| i}
+    end
+    
+    it 'should convert all values to strings' do
+      @typed_ary.each do |e|
+        e.should be_an_instance_of(String)
+      end
+    end
+  end
+end
